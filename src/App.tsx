03382e39536b66fe082278dcfc52671f25c0daa9
TheIns07 <arturoinscreup2401@hotmail.com>
2023-10-25 19:56:14 -0700

    Fetch completado. Conexion entre aplicaciones completado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CategoryDTO } from './models/CategoryDTO';
+
 
 function App() {
+  const [categories, setCategories] = useState<Array<CategoryDTO>>([]);
+
+  function getAllCategories(){
+    return fetch('https://localhost:7131/api/Category')
+    .then(res => res.json())
+    .then(data => setCategories(data))
+    .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getAllCategories();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {categories.map((category, index) => {
+        return (
+          <div>
+          <h1>{index}</h1>
+          <h1>{category.name}</h1>
+          </div>
+        )
+      })}
     </div>
   );
 }
+
+
 
 export default App;
